8f8d307ade28031fc2126593bd6e10e110ac6eb6
ohyuchan <oyuchan50@gmail.com>
2022-04-13 18:10:29 +0900

    study python

---
+++ 04/13.py
@@ -0,0 +1,26 @@
+# \n : 문장네에서 줄바꿈
+print("백문이 불여일견\n백견이 불여일타")#백번 듣는 것보다 한번 보는겐 낫고 백번 보는 것보다 한번 처보는게 더 낫다
+
+print("저는 '나도코딩' 입니다")
+
+print("저는 \"나도코딩\"입니다")
+print('저는 \'나도코딩\'입니다')
+
+
+# \\ : 문장네에서 \
+print("C:\\Users\\user\\OneDrive\\바탕 화면\\Mobile Robot")
+
+# \r : 커서를 맨 앞으로 이동
+
+print("Red Apple\rPine")
+
+# \b : 백스페이스(한글자 삭제)
+print("Redd\bApple")
+
+# \t : 탭
+
+print("Red\tApple")
+
+
+
+
