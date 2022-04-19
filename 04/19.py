727c70250c358948f3c48d3f8980109183225d5b
ohyuchan <oyuchan50@gmail.com>
2022-04-19 21:39:19 +0900

    study python

---
+++ 04/19.py
@@ -0,0 +1,33 @@
+# 리스트[]
+# 지하철 칸별로 10명 ,20명 , 30명
+
+subway1=10
+subway2=20
+subway3=30
+
+subway=[10,20,30]
+print(subway)
+
+subway=["유재석","조세호","박명수"]
+print(subway)
+
+
+#현재 리스트의 조세호의 위치
+print(subway.index("조세호"))
+
+#하하씨가 다음 정류장애서 다음 칸에 탐
+subway.append("하하")
+print(subway)
+
+# 정현돈씨를 유재석 /조세호
+subway.insert(1,"정현돈")
+print(subway)
+
+# 지하철에 있는 사람을 한 명씩 뒤애서 꺼냄
+print(subway.pop())
+print(subway)
+
+#정렬도 가능
+num_list=[5,4,3,2,1]
+num_list.sort()
+print(num_list)
